a261e38efadec8713a2fd4cdcfb7127d7d1352b0
Andrew-cod <andrei.arapu@student.upt.ro>
2023-11-13 18:08:09 +0200

    Inceperea proiectului

---
+++ calul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void afis(){
+
+}
+
+int solve(){
+
+}
+
+int main(){
+
+    printf("salut acesta este inceputul de proiesc ce este incarcat pe github");
+
+
+
+
+    return 0;
+}
