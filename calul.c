dc8112f4b8f521fb6302ddc0f34915c2be3ad59e
Andrew-cod <andrei.arapu@student.upt.ro>
2023-11-13 19:17:56 +0200

    Codul a fost schimbat incat tabla este de o marime variabila si startul este din pozitia 0 0

--- calul.c
+++ calul.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 8
 
 //posibilitatile de miscare pe tabla de sah
 int moveX[8] = {2, 1, -1, -2, -2, -1, 1, 2};
 int moveY[8] = {1, 2, 2, 1, -1, -2, -2, -1};
 
-int valid(int x, int y, int sol[N][N])
+int valid(int x, int y, int n, int **sol)
 {
-    return (x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == -1);
+    return (x >= 0 && x < n && y >= 0 && y < n && sol[x][y] == -1);
 }
 
 
-int solve(int x, int y, int movei, int sol[N][N])
+int solve(int x, int y, int n, int movei, int **sol)
 {
     int nextX, nextY;
 
-    if (movei == N * N)
+    if (movei == n * n)
         return 1;
 
     // incercam toate miscarile ale calului 
     for (int k = 0; k < 8; k++){
         nextX = x + moveX[k];
         nextY = y + moveY[k];
-        if (valid(nextX, nextY, sol)) //verificam daca miscarea este valida (suntem pe pozitie existenta)
+        if (valid(nextX, nextY, n, sol)) //verificam daca miscarea este valida (suntem pe pozitie existenta)
         {
             sol[nextX][nextY] = movei; //marcam pozitia
-            if (solve(nextX, nextY, movei + 1, sol)) //verificam daca avem o solutie mai departe
+            if (solve(nextX, nextY, n, movei + 1, sol)) //verificam daca avem o solutie mai departe
                 return 1;
             else
                 sol[nextX][nextY] = -1; // in caz ca nu avem o solutie viitoare din aceasta pozitie
@@ -38,7 +37,7 @@ int solve(int x, int y, int movei, int sol[N][N])
     return 0;
 }
 
-void afis(int sol[N][N],int n){
+void afis(int n, int **sol){
     // Afisează soluția găsită
     for (int i = 0; i < n; i++)
     {   
@@ -51,25 +50,38 @@ void afis(int sol[N][N],int n){
 
 
 int main(){
-    int sol[N][N];
+    int n;
+    scanf("%d",&n);
+    
+    //alocare dinamica de memorie
+    int **sol=(int**)malloc(sizeof(int*)*n);
+    for(int i=0;i<n;i++)
+        sol[i]=(int*)malloc(sizeof(int)*n);
+
+
     int i=0,j=0;
     // initializam toata tabla cu -1 (prin conventie Calul inca nu a trecut prin pozitia -1)
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
             sol[i][j] = -1;
     sol[0][0] = 0;
 
     // Începeți călătoria călătorului de la (0, 0)
-    if (solve(0, 0, 1, sol) == 0)
+    if (solve(0, 0, n, 1, sol) == 0)
     {
         printf("Nu există soluție.\n");
         return 0;
     }
     else
     {
-        afis(sol,N);
+        afis(n, sol);
         
     }
 
+    //eliberare de memorie
+    for(int i=0;i<n;i++){
+        free(sol[i]);
+    }
+    free(sol);
     return 0;
 }
